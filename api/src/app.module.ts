6ba6085352a9f941a0aca3c22276685a11687421
yurihartmann <yurihartmann0607@gmail.com>
2023-11-08 20:38:43 -0300

    development

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { StoresModule } from './stores/stores.module';
 
 @Module({
-  imports: [StoresModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
